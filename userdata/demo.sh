9cf17814e5ea2c31577e0c472f73c23101301884
nowkoai <hyamasit@gmail.com>
2022-08-22 15:57:34 +0900

    0822-init

--- userdata/demo.sh
+++ userdata/demo.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # yum update -y
 sudo yum install -y httpd
-echo "<p>" >> /var/www/html/index.html
-cat /etc/system-release >> /var/www/html/index.html
 
-echo "<br>" >> /var/www/html/index.html
-uname -n >> /var/www/html/index.html
+cat << EOM > /var/www/html/index.html
+<html>
+  <head><title>Meow!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+  <!-- BEGIN -->
+  <center><img src="http://placekitten.com/600/400"></img></center>
+  <center><h2>デモだよ</h2></center>
+  デモサイトです！
+  <!-- END -->
+  </div>
+  </body>
+</html>
+EOM
 
-echo "<br>" >> /var/www/html/index.html
-echo "→ テスト用AWSサーバが起動しました！" >> /var/www/html/index.html
-echo "</p>" >> /var/www/html/index.html
+# ---------------------------------------------
+# echo "<p>" >> /var/www/html/index.html
+# cat /etc/system-release >> /var/www/html/index.html
+#
+# echo "<br>" >> /var/www/html/index.html
+# uname -n >> /var/www/html/index.html
+#
+# echo "<br>" >> /var/www/html/index.html
+# echo "→ テスト用AWSサーバが起動しました！" >> /var/www/html/index.html
+# echo "</p>" >> /var/www/html/index.html
+# ---------------------------------------------
 
 sudo systemctl start httpd
 # sudo systemctl enable httpd
