05349475ebad73d1945f16ce6c913511e0edfeeb
nowkoai <hyamasit@gmail.com>
2022-08-22 18:17:16 +0900

    2

--- userdata/demo.sh
+++ userdata/demo.sh
@@ -4,30 +4,31 @@ sudo yum install -y httpd
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>HashiCat</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
-  <!-- BEGIN -->
   <center><img src="http://placekitten.com/600/400"></img></center>
-  <center><h2>デモだよ</h2></center>
-  デモサイトです！
-  <!-- END -->
-  </div>
-  </body>
-</html>
+  <center><h2>おつかれさまです</h2></center>
+  <center><h2>TerraformでデプロイしたWEBサイトになります！</h2></center>
 EOM
 
-# ---------------------------------------------
-# echo "<p>" >> /var/www/html/index.html
-# cat /etc/system-release >> /var/www/html/index.html
-#
-# echo "<br>" >> /var/www/html/index.html
-# uname -n >> /var/www/html/index.html
-#
-# echo "<br>" >> /var/www/html/index.html
-# echo "→ テスト用AWSサーバが起動しました！" >> /var/www/html/index.html
-# echo "</p>" >> /var/www/html/index.html
-# ---------------------------------------------
+---------------------------------------------
+echo "<p>" >> /var/www/html/index.html
+cat /etc/system-release >> /var/www/html/index.html
+
+echo "<br>" >> /var/www/html/index.html
+uname -n >> /var/www/html/index.html
+
+echo "<br>" >> /var/www/html/index.html
+echo "→ テスト用AWSサーバが起動しました！" >> /var/www/html/index.html
+echo "</p>" >> /var/www/html/index.html
+---------------------------------------------
+
+cat << EOM > /var/www/html/index.html
+</div>
+</body>
+</html>
+EOM
 
 sudo systemctl start httpd
 # sudo systemctl enable httpd
