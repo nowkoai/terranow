5fd92e46027b7c61d3b3157978c59b62c4c308c8
nowkoai <hyamasit@gmail.com>
2022-08-23 18:24:21 +0900

    now

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Server" {
   count         = 1
 
   # インスタンス指定
-  ami           = "ami-0701e21c502689c31"
+  ami           = "ami-090fa75af13c156b4"
 
   instance_type = var.instance_type
 
